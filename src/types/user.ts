a433d086288aafd92c1868b5651e8aa4deb21b5f
Vlad Zusko <vladzvu11@gmail.com>
2021-11-15 16:17:33 +0200

    Added user type

---
+++ src/types/user.ts
@@ -0,0 +1,48 @@
+export interface IUserBioLink {
+  link: string;
+  risk: number;
+}
+
+export interface IUserStats {
+  followerCount: number;
+  followingCount: number;
+  heart: number;
+  heartCount: number;
+  videoCount: number;
+  diggCount: number;
+}
+
+export interface IUserData {
+  id: string;
+  shortId: string;
+  uniqueId: string;
+  nickname: string;
+  avatarLarger: string;
+  avatarMedium: string;
+  avatarThumb: string;
+  signature: string;
+  createTime: number;
+  verified: boolean;
+  secUid: string;
+  ftc: boolean;
+  relation: number;
+  openFavorite: boolean;
+  bioLink: IUserBioLink;
+  commentSetting: number;
+  duetSetting: number;
+  stitchSetting: number;
+  privateAccount: boolean;
+  secret: boolean;
+  isADVirtual: boolean;
+  roomId: string;
+}
+
+export interface IUserItem {
+  id: string;
+}
+
+export interface IUser {
+  user: IUserData;
+  stats: IUserStats;
+  items: IUserItem[];
+}
