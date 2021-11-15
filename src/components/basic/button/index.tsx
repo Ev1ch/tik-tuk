20c8d9d636f757dba6face9c2343dea12c62a2c1
Vlad Zusko <vladzvu11@gmail.com>
2021-11-15 13:11:33 +0200

    Added button component

---
+++ src/components/basic/button/index.tsx
@@ -0,0 +1,29 @@
+import React, { MouseEvent } from 'react';
+import clsx from 'clsx';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { IconProp } from '@fortawesome/fontawesome-svg-core';
+import styles from './button.module.scss';
+
+interface IButtonProps {
+  icon?: IconProp;
+  text?: string;
+  className?: string;
+  onClick?: (event: MouseEvent<HTMLButtonElement>) => void;
+}
+
+const Button = ({ icon, text, className, onClick }: IButtonProps) => {
+  const onClickHandler = (event: MouseEvent<HTMLButtonElement>) => {
+    if (onClick) {
+      onClick(event);
+    }
+  };
+
+  return (
+    <button className={clsx(styles.button, className)} onClick={onClickHandler}>
+      {icon && <FontAwesomeIcon icon={icon} />}
+      {text}
+    </button>
+  );
+};
+
+export default Button;
