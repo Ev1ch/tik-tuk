4726e98bd127605eb2d2c242ea5cb1f3fbd5e2d3
Vlad Zusko <vladzvu11@gmail.com>
2021-11-15 13:41:15 +0200

    Added basic routes

--- src/common/enums/routes.ts
+++ src/common/enums/routes.ts
@@ -1,3 +1,7 @@
-const Routes = {};
+const Routes = {
+  HOME: '/',
+  USERS: (username?: string) => (username ? `/users/${username}` : '/users/:username'),
+  TAGS: (tag?: string) => (tag ? `/tags/${tag}` : '/tags/:tag'),
+};
 
 export default Routes;
