9a68baf9e0bce43794fd2a2f85cb5d5ba2415a64
Vlad Zusko <vladzvu11@gmail.com>
2021-11-15 13:37:46 +0200

    Added basic logo component

---
+++ src/components/basic/logotype/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import clsx from 'clsx';
+import styles from './logotype.module.scss';
+
+interface ILogotypeProps {
+  className?: string;
+}
+
+const Logotype = ({ className }: ILogotypeProps) => {
+  return <p className={clsx(styles.logotype, className)}>Tik Tuk</p>;
+};
+
+export default Logotype;
