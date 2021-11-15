4c910e23760cd9f5f4c6171b14def28983959306
Vlad Zusko <vladzvu11@gmail.com>
2021-11-15 13:25:01 +0200

    Fixed avatar props interface

--- src/components/basic/avatar/index.tsx
+++ src/components/basic/avatar/index.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import clsx from 'clsx';
 import styles from './avatar.module.scss';
 
-interface IAvatar {
+interface IAvatarProps {
   image: string;
   alt?: string;
   className?: string;
 }
 
-const Avatar = ({ image, alt, className }: IAvatar) => {
+const Avatar = ({ image, alt, className }: IAvatarProps) => {
   return (
     <div className={clsx(styles.avatar, className)}>
       <img src={image} alt={alt} className={styles.image} />
