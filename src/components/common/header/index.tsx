30b3434dc92ad3dbdcc17866f7b47449b8ea1945
Vlad Zusko <vladzvu11@gmail.com>
2021-11-16 18:28:42 +0200

    Added header logo link

--- src/components/common/header/index.tsx
+++ src/components/common/header/index.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import clsx from 'clsx';
+import { useHistory } from 'react-router';
 import { Logotype } from 'components';
 import styles from './header.module.scss';
+import { Routes } from 'common';
 
 const Header = () => {
+  const history = useHistory();
+
+  const onLogoClick = () => {
+    history.push(Routes.TRENDING);
+  };
+
   return (
-    <header className={styles.header}>
+    <header className={styles.header} onClick={onLogoClick}>
       <div className={clsx('container', styles.container)}>
-        <Logotype />
+        <Logotype className={styles.logotype} />
       </div>
     </header>
   );
